6b9b3d4573dd4072f90c3f0980699b9d7285315b
Rieskyfabs <rieskyfabs@gmail.com>
2024-11-04 14:16:03 +0700

    tambah post, dan kategori post

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -11,52 +11,64 @@
         @endif
 
         <div class="mb-3">
-            <a href="{{ route('posts.create') }}" class="btn btn-primary">+ posts</a>
+            <a href="{{ route('posts.create') }}" class="btn btn-primary">+ Tambah Post</a>
         </div>
 
+        <h4 class="card-title">Data Posts</h4>
+        <h6 class="card-subtitle">Di sini Anda bisa melihat data post yang tersedia.</h6>
+
         <div class="table-responsive">
-            <table class="table table-bordered table-hover">
-                <thead>
+            <table class="table table-bordered table-striped table-hover">
+                <thead class="table-light">
                     <tr>
-                        <th scope="col">No</th>
-                        <th scope="col">Judul</th>
-                        <th scope="col">Kategori</th>
-                        <th scope="col">Petugas</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Dibuat pada tanggal</th>
-                        <th scope="col">Aksi</th>
+                        <th>No</th>
+                        <th>Judul</th>
+                        <th>Kategori</th>
+                        <th>Petugas</th>
+                        <th>Status</th>
+                        <th>Dibuat pada Tanggal</th>
+                        <th>Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
                     @forelse($posts as $index => $post)
-                    <tr>
-                        <th scope="row">{{ $index + 1 }}</th>
-                        <td>{{ $post->title }}</td>
-                        <td>{{ $post->category->title ?? 'Tidak ada kategori' }}</td>
-                        <td>{{ $post->user->name ?? 'Tidak ada petugas' }}</td>
-                        <td>
-                            @if (Str::lower($post->status) == 'publish')
-                                <span class="badge bg-success text-white">{{ Str::lcfirst($post->status) }}</span>
-                            @else
-                                <span class="badge bg-warning text-white">{{ $post->status }}</span>
-                            @endif
-                        </td>
-                        <td>{{ \Carbon\Carbon::parse($post->created_at)->format('d M Y') }}</td>
-                        <td class="d-flex">
-                            <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-warning btn-sm me-2">Edit</a>
-                            <form action="{{ route('posts.destroy', $post->id) }}" method="POST" style="display:inline;">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-                            </form>
-                        </td>
-                    </tr>
+                        <tr>
+                            <td>{{ $index + 1 }}</td>
+                            <td>{{ $post->title }}</td>
+                            <td>{{ $post->category->title ?? 'Tidak ada kategori' }}</td>
+                            <td>{{ $post->user->name ?? 'Tidak ada petugas' }}</td>
+                            <td>
+                                <span class="badge {{ strtolower($post->status) == 'publish' ? 'bg-success' : 'bg-warning' }} text-white">
+                                    {{ ucfirst($post->status) }}
+                                </span>
+                            </td>
+                            <td>{{ \Carbon\Carbon::parse($post->created_at)->format('d M Y') }}</td>
+                            <td class="d-flex">
+                                <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-warning btn-sm me-2">Edit</a>
+                                <form action="{{ route('posts.destroy', $post->id) }}" method="POST" style="display:inline;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
                     @empty
-                    <tr>
-                        <td colspan="7" class="text-center">Tidak ada data tersedia</td>
-                    </tr>
+                        <tr>
+                            <td colspan="7" class="text-center text-muted">Tidak ada data tersedia</td>
+                        </tr>
                     @endforelse
                 </tbody>
+                <tfoot class="table-light">
+                    <tr>
+                        <th>No</th>
+                        <th>Judul</th>
+                        <th>Kategori</th>
+                        <th>Petugas</th>
+                        <th>Status</th>
+                        <th>Dibuat pada Tanggal</th>
+                        <th>Aksi</th>
+                    </tr>
+                </tfoot>
             </table>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -61,10 +61,10 @@ Route::prefix('admin/categories')->group(function () {
 
 //route untuk crud post
 Route::prefix('admin/posts')->group(function () {
-    Route::get('/', [CategoryController::class, 'index'])->name('posts.index');
-    Route::get('/create', [CategoryController::class, 'create'])->name('posts.create');
-    Route::post('/', [CategoryController::class, 'store'])->name('posts.store');
-    Route::get('/{id}/edit', [CategoryController::class, 'edit'])->name('posts.edit');
-    Route::put('/{id}', [CategoryController::class, 'update'])->name('posts.update');
-    Route::delete('/{id}',[CategoryController::class, 'destroy'])->name('posts.destroy');
+    Route::get('/', [PostController::class, 'index'])->name('posts.index');
+    Route::get('/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/{id}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/{id}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/{id}',[PostController::class, 'destroy'])->name('posts.destroy');
 })->middleware('auth');
